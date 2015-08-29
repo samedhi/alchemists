79feeb6efb74510960ac90994ae5114b3141d659
Stephen Cagle <samedhi@gmail.com>
2015-08-29 15:32:41 -0700

    Added the ingredients

--- src/cljs/alchemist/core.cljs
+++ src/cljs/alchemist/core.cljs
@@ -17,6 +17,9 @@
 
 (def CHEMICALS #{:r-g+B- :r+g-B+ :r+G-b- :r-G+b+ :R-g-b+ :R+g+b- :R-G-B- :R+G+B+})
 
+(def INGREDIENTS #{:ivy :chicken-foot :mushroom :forget-me-not
+                   :mandrake :scorpion :toad :crow-feather})
+
 (def STATE (atom {}))
 
 ;; FUNCTIONS
