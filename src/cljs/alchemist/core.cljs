d208be0216d53a32928a94349c346ab3a15232f3
Stephen Cagle <samedhi@gmail.com>
2015-08-29 15:30:06 -0700

    I build some resources for the data

--- src/cljs/alchemist/core.cljs
+++ src/cljs/alchemist/core.cljs
@@ -11,8 +11,57 @@
 
 (enable-console-print!)
 
-(def STATE
-  (atom {}))
+;; DATA
+
+(def COLORS [:red :green :blue])
+
+(def CHEMICALS #{:r-g+B- :r+g-B+ :r+G-b- :r-G+b+ :R-g-b+ :R+g+b- :R-G-B- :R+G+B+})
+
+(def STATE (atom {}))
+
+;; FUNCTIONS
+
+(defn chemical? [chemical]
+  (if (keyword? chemical)
+    (let [c (name chemical)
+          [m & _] (re-find #"(R|r)(\+|-)(G|g)(\+|-)(B|b)(\+|-)" c)]
+      (and (keyword? chemical) (= m c)))
+    false))
+
+(defn detail? [detail]
+  (and (every? (fn [k] (contains? detail k)) COLORS)
+       (every?
+        (fn [k]
+          (let [s (get-in detail [k :large?])] (or (true? s) (false? s))))
+        COLORS)
+       (every?
+        (fn [k]
+          (let [s (get-in detail [k :positive?])] (or (true? s) (false? s))))
+        COLORS)))
+
+(defn chemicals [detail]
+  {:pre [(detail? detail)]
+   :post [(chemical? %)]}
+  (keyword
+   (str
+    (if (-> detail :red   :large?) "R" "r")
+    (if (-> detail :red   :large?) "+" "-")
+    (if (-> detail :green :large?) "G" "g")
+    (if (-> detail :green :large?) "+" "-")
+    (if (-> detail :blue  :large?) "B" "b")
+    (if (-> detail :blue  :large?) "+" "-"))))
+
+(defn details [chemical]
+  {:pre [(chemical? chemical)]
+   :post [(detail? %)]}
+  (apply
+   merge
+   (for [[c s p] (map cons [:red :green :blue] (partition 2 (name chemical)))
+         :let [capitalized? (=  (.toUpperCase s) s)
+               polarity (= "+" p)]]
+     {c {:large? capitalized? :positive? polarity}})))
+
+;; OM
 
 (defn view [{:keys [modal-view thread-view?] :as app} owner]
   (reify
