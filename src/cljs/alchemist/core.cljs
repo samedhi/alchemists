8997233a513e1b5bdc126bc63be4ac4cc16b554c
Stephen Cagle <samedhi@gmail.com>
2015-08-30 16:46:24 -0700

    train yourself to know what potion will generate

--- src/cljs/alchemist/core.cljs
+++ src/cljs/alchemist/core.cljs
@@ -1,6 +1,7 @@
 (ns alchemist.core
   (:require
    [cljs.core.async :as async :refer [>! <! put! chan dropping-buffer timeout close!]]
+   [clojure.string :refer [join]]
    [om.core :as om :include-macros true]
    [om.dom :as dom :include-macros true]
    goog.dom
@@ -14,12 +15,15 @@
 
 (def COLORS [:red :green :blue])
 
-(def AlCHEMICALS #{:r-g+B- :r+g-B+ :r+G-b- :r-G+b+ :R-g-b+ :R+g+b- :R-G-B- :R+G+B+})
+(def ALCHEMICALS #{:r-g+B- :r+g-B+ :r+G-b- :r-G+b+ :R-g-b+ :R+g+b- :R-G-B- :R+G+B+})
 
 (def INGREDIENTS #{:ivy :chicken-foot :mushroom :forget-me-not
                    :mandrake :scorpion :toad :crow-feather})
 
-(def STATE (atom {}))
+(def STATE (atom {:guess-result
+                  {:alchemical-x :R-G-B-
+                   :alchemical-y :R+G+B+
+                   :visible? false}}))
 
 ;; FUNCTIONS
 
@@ -60,40 +64,83 @@
                polarity (= "+" p)]]
      {c {:large? capitalized? :positive? polarity}})))
 
-(defn mix [x y]
+(defn mix-into-potion [x y]
   (let [x (if (detail? x) detail (detail x))
         y (if (detail? y) detail (detail y))
         potion-created? (fn [[_ b c]]
                           (and (= (:positive? b) (:positive? c))
                                (not= (:large? b) (:large? c))))
-        rs (->> (map #(vector %1 (get x %1) (get y %1)) COLORS)
-                (filter potion-created?)
-                (map first))]
+        [[r m _] :as rs] (->> (map #(vector %1 (get x %1) (get y %1)) COLORS)
+                                (filter potion-created?))]
     (assert (-> rs count (<= 1)) "It should yield at most one potion (or a tasty soup)")
     (if (empty? rs)
-      :neutral
-      (first rs))))
+      {:color :neutral}
+      {:color r :positive? (:positive? m)})))
+
+(defn alchemical-to-image [alchemical]
+  (let [alchemical (detail alchemical)
+        fx (fn [color]
+             (let [{:keys [positive? large?]} (get alchemical color)]
+               (str (-> color name first)
+                    (if large? "l" "s")
+                    (if positive? "p" "n"))))]
+    (str "image/" (join "_" (map fx COLORS)) ".png")))
+
+(defn potion-to-image [{:keys [positive? color]}]
+  (str
+   "image/"
+   (join
+      "_"
+      (remove
+       nil?
+       ["potion"
+        (name color)
+        (cond positive? "positive" (false? positive?) "negative")]))
+   ".png") )
 
 ;; OM
 
-(defn view [{:keys [modal-view thread-view?] :as app} owner]
+
+(defn guess-result-view [{:keys [alchemical-x alchemical-y visible?] :as app}]
   (reify
     om/IRender
     (render [_]
       (dom/div
-       #js {:className "root"}
-       (dom/img #js {:className "ingredient" :src "image/ivy.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/chicken-foot.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/mushroom.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/forget-me-not.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/mandrake.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/scorpion.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/toad.jpg"})
-       (dom/img #js {:className "ingredient" :src "image/crow-feather.jpg"})))))
+       #js {:className "guess-result"}
+       (dom/div
+        #js {:className "refresh unselectable"}
+        (dom/i #js {:className "material-icons large"
+                   :onClick #(let [x (rand-nth (vec ALCHEMICALS))
+                                   y (rand-nth (vec (disj ALCHEMICALS x)))]
+                               (om/update! app {:alchemical-x x :alchemical-y y}))}
+              "refresh"))
+       (dom/div
+        #js {:className "equation"}
+        (dom/img #js {:className "alchemical" :src (alchemical-to-image alchemical-x)})
+        (dom/i #js {:className "material-icons large"} "add")
+        (dom/img #js {:className "alchemical" :src (alchemical-to-image alchemical-y)})
+        (dom/i #js {:className "material-icons large"} "arrow_forward")
+        (dom/div
+         #js {:className "result unselectable"}
+         (if visible?
+           (dom/img #js {:className "potion"
+                         :src (potion-to-image (mix-into-potion alchemical-x alchemical-y))})
+           (dom/div #js {:className "reveal grow"
+                         :onClick #(om/update! app [:visible?] true)}
+                    (dom/i #js {:className "material-icons large"} "help")
+                    "Click to Reveal"))))))))
+
+(defn view [{:keys [guess-result] :as app} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div
+       #js {:className "root container"}
+       (om/build guess-result-view guess-result)))))
 
 (om/root
  view
  STATE
  {:target (goog.dom.getElement "app")})
 
-(add-watch STATE :state-change (fn [_ _ _ n] (println (dissoc n :groups))))
+(add-watch STATE :state-change (fn [_ _ o n] (when (not= o n) (println n))))
